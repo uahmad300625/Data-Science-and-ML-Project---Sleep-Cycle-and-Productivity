19a0845d99deabce479488245952a4750de4ef93
uahmad300625 <umar.ahmad2242@gmail.com>
2025-04-11 11:18:51 -0500

    Adding the Code and Data file to repository

---
+++ Productivity_Code.R
@@ -0,0 +1,67 @@
+
+library(dplyr)
+
+#Loading and Cleaning data
+
+cleaned_data <- sleep_cycle_productivity %>%
+  na.omit() %>%
+  select(-Person_ID, -Date, -Mood.Score, -Stress.Level) %>%
+  mutate(
+    # Convert gender to factor 
+    Gender = as.factor(Gender),
+    
+    # Create a binary variable to do Logistic Regression.
+    Productivity_Binary_Score = factor(
+      ifelse(Productivity.Score >= 6, "High", "Low"), 
+      levels = c("Low", "High")
+    ),
+    
+  )
+
+
+# Run logistic regression
+initial_lm <- glm(Productivity_Binary_Score ~ Age + Gender + Total.Sleep.Hours + Exercise..mins.day. 
+                  + Caffeine.Intake..mg. + Screen.Time.Before.Bed..mins. + Work.Hours..hrs.day.+ Sleep.Quality, 
+                 family = binomial, 
+                 data = cleaned_data)
+summary(initial_lm)
+
+
+#simplified_model
+
+refined_model <- glm(Productivity_Binary_Score ~ Total.Sleep.Hours, 
+                     family = binomial, 
+                     data = cleaned_data)
+summary(refined_model)
+
+
+#Finding MSE for the Refined Model
+
+mse = rep(0,10)
+for (i in 1:10){
+  set.seed(i)
+  
+  train_index = sample(1:nrow(cleaned_data), size = 0.8 *nrow(cleaned_data))
+  train_data = cleaned_data[train_index,]
+  test_data = cleaned_data[-train_index,]
+  
+  refined_model <- glm(Productivity_Binary_Score ~ Total.Sleep.Hours, 
+                       family = binomial, 
+                       data = train_data)
+  
+  predictions = predict(refined_model, newdata = test_data, type = "response")
+  actuals = ifelse(test_data$Productivity_Binary_Score == "High", 1, 0)
+  
+  mse[i] = mean((predictions - actuals)^2)
+  print(mse[i])
+}
+
+mean_mse = mean(mse)
+print(mean_mse)
+
+
+
+#Linear Regression Model - not a good model, only one significant variable
+
+#initial_lm <- lm(cleaned_data$Productivity_Binary_Score ~., data = cleaned_data)
+#summary(initial_lm)
